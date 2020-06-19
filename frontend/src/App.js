fa0af67a936ae4c169049ee4a83c3f000e51a14a
Matan18 <mateusandriola18@gmail.com>
2020-06-19 12:39:06 -0300

    NodeJS / ReactJS ready, ReactNative init

---
+++ frontend/src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import api from "../src/services/api";
+import './app.css'
+
+function App(){
+    return (
+        <h1>Hello GoStack</h1>
+    )
+}
+export default App;
